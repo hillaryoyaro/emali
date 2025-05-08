7a9552d880513ce514b155b2332e3c65701b2f12
oyaro <hillaryoyaroh@gmail.com>
2025-05-08 13:44:00 +0300

    #24-creating-search-component-header

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -11,10 +11,12 @@ import {
   SelectValue,
 } from '../../ui/select'
 import { APP_NAME } from '@/lib/constants'
-const categories = ['Men', 'Women', 'Kids', 'Accessories']
+import { getAllCategories } from '@/lib/actions/product.actions'
+
 
 
 export default async function Search() {
+  const categories = await getAllCategories()
    return (
     <form action='/search' method='GET' className='flex  items-stretch h-10 '>
       <Select name='category'>
